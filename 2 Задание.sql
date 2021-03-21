0e97d24fb53484918c9f74af24509cd1c579e44f
Egor Taran <64767049+egortaran@users.noreply.github.com>
2021-03-21 18:50:41 -0400

    Update 2 Задание.sql

--- 2 Задание.sql
+++ 2 Задание.sql
@@ -1,26 +1,26 @@
 SELECT from_user_id as 'Пользователь', to_user_id as 'Друг', SUM(count) as 'Сообщения' 
 FROM(
 	SELECT CASE 
-		   		when from_user_id != 210 then to_user_id 
-				when to_user_id then from_user_id
-				end as 'from_user_id',
-		   CASE 
-			    when to_user_id = 210 then from_user_id 
-				when from_user_id then to_user_id
-				end as 'to_user_id',
-		   COUNT(*) as count
+			when from_user_id != 210 then to_user_id 
+			when to_user_id then from_user_id
+			end as 'from_user_id',
+	       CASE 
+			when to_user_id = 210 then from_user_id 
+			when from_user_id then to_user_id
+			end as 'to_user_id',
+	       COUNT(*) as count
 	FROM messages m
 	WHERE 
 		from_user_id in (
-	    	SELECT fr.initiator_user_id
-	    	from friend_requests fr 
-	    	where fr.status = 'approved' AND fr.initiator_user_id = 210
+	    		SELECT fr.initiator_user_id
+	    		from friend_requests fr 
+	    		where fr.status = 'approved' AND fr.initiator_user_id = 210
 	  	)
-	  OR 
+	OR 
 	  	to_user_id in (
 	  		SELECT fr.target_user_id
-	    	from friend_requests fr 
-	    	where fr.status = 'approved' AND fr.target_user_id = 210
+	    		from friend_requests fr 
+	    		where fr.status = 'approved' AND fr.target_user_id = 210
 	  	)
 	GROUP BY from_user_id, to_user_id
 )as results1
