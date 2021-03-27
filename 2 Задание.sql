5f5df4d8d462f036c0860701f4805f32ff3efdab
Egor Taran <64767049+egortaran@users.noreply.github.com>
2021-03-27 17:09:31 -0400

    Update 2 Задание.sql

--- 2 Задание.sql
+++ 2 Задание.sql
@@ -1,8 +1,6 @@
 -- Выведите список товаров products и разделов catalogs, который соответствует товару.
 -- Честно говоря, плохо понял смысл задания, если оно решено неверно, то напишите, пожалуйста, в телеграм @Taran_27
 
-SELECT id, name, (SELECT c.name 
-		  FROM catalogs c 
-		  WHERE c.id = p.catalog_id 
-		 )as 'catalog'	
-FROM products p ;
+SELECT p.name, c.name
+FROM products p JOIN catalogs c 
+ON p.catalog_id = c.id ;
